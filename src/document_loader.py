6c1a7336e49013e4f94010e5344f7252d361b83a
Aditya Khemka <khemkaaditya48@gmail.com>
2026-07-15 00:37:49 +0530

    document splitter (using pdf)

--- src/document_loader.py
+++ src/document_loader.py
@@ -2,6 +2,9 @@
 
 from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_core.documents import Document
 
 
 def load_pdf(pdf_path: str | Path):
@@ -17,3 +20,21 @@ def load_pdf(pdf_path: str | Path):
     print(f"Loaded {len(documents)} page(s).")
 
     return documents
+
+
+
+def document_splitter():
+   
+    docs = load_pdf("data/sample.pdf")
+    splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+
+    # split the docs
+    split_docs = splitter.split_documents(docs)
+
+    print(f"Split into {len(split_docs)} chunks")
+    print(f"\nFirst chunk metadata: {split_docs[0].metadata}")
+    print(f"First chunk content: {split_docs[0].page_content[:200]}...")
+
+
+if __name__ == "__main__":
+    document_splitter()
